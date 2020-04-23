667bb7b4e36367ff090188f35dfa406614a31aa1
Nadia M <siraniadam@me.com>
2020-04-23 10:01:19 +0200

    Initial Commit

---
+++ App5OcrAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App5OcrAPI
+//
+//  Created by Nadia Mettioui on 23/04/2020.
+//  Copyright © 2020 Nadia Mettioui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
